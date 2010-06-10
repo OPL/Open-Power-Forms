2955eac59bf3818b2f9b6cb3951b8dbe1e0a24f8
eXtreme <jacek.jedrzejewski@gmail.com>
2010-06-10 23:29:54 +0200

    SVN keyword cleanup

--- lib/View/Instruction/Form.php
+++ lib/View/Instruction/Form.php
@@ -10,7 +10,6 @@
  * Copyright (c) Invenzzia Group <http://www.invenzzia.org>
  * and other contributors. See website for details.
  *
- * $Id$
  */
 
 class Opf_View_Instruction_Form extends Opt_Compiler_Processor
