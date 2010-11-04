443791796f1263ff7f393662309ed84298e103c6
zyxist <nightstorm@tlen.pl>
2010-11-04 17:11:47 +0000

    Adding Text widget.

---
+++ lib/Opf/Widget/Text.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ *  OPEN POWER LIBS <http://www.invenzzia.org>
+ *  ------------------------------------------
+ *
+ * This file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE. It is also available through
+ * WWW at this URL: <http://www.invenzzia.org/license/new-bsd>
+ *
+ * Copyright (c) Invenzzia Group <http://www.invenzzia.org>
+ * and other contributors. See website for details.
+ */
+
+namespace Opf\Widget;
+
+use Opf\Design;
+use Opt_Function;
+
+/**
+ * The class represents a text area widget.
+ * @package Widgets
+ */
+class Text extends Component
+{
+	/**
+	 * Returns the unique component type name.
+	 * @return string
+	 */
+	public function getComponentName()
+	{
+		return 'text';
+	} // end getComponentName();
+	/**
+	 * Displays the textarea component.
+	 *
+	 * @param array $attributes The opt:display attribute list.
+	 */
+	public function display($attributes = array())
+	{
+		$attributes = array(
+			'name' => $this->_item->getFullyQualifiedName(),
+			'class' => Opf_Design::getClass('input', $this->_item->isValid()),
+			// TODO: Move to design or something like that.
+			'rows' => 5,
+			'cols' => 50
+		);
+		echo '<textarea '.Opt_Function::buildAttributes($attributes).'>'.$this->_item->getDisplayedValue().'</textarea>';
+	} // end display();
+} // end Opf_Widget_Text;
